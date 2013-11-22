37bc47e28b1b511c99c9c53995d49272293a10a8
RobertAlblas <robertalblas1993@gmail.com>
2013-11-22 14:27:30 +0100

    deleted spawnlevel, added TODO's in soundset

--- Nordland/src/robertalblas/nordland/resource/sound/SoundSet.java
+++ Nordland/src/robertalblas/nordland/resource/sound/SoundSet.java
@@ -1,11 +1,6 @@
 package robertalblas.nordland.resource.sound;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 import robertalblas.nordland.resource.ResourceSet;
-import robertalblas.nordland.resource.sound.loader.SoundLoader;
 
 public class SoundSet extends ResourceSet {
 
@@ -15,26 +10,9 @@ public class SoundSet extends ResourceSet {
 
 	@Override
 	public void load() {
-		SoundLoader soundLoader = new SoundLoader();
-		
-		"/sound/" + getFile()
-		//for every sound in folder
-			//do soundloader
-			//add sound to resourceset
-		
-		
+		//TODO: Read XML file
+		//TODO: Load sound files
+
 	}
-	
-	private List<String> getFilesInDirectory(String directory) {
-		List<String> files = new ArrayList<String>();
-		File directoryFile = new File(directory);
-		
-	    for (File fileEntry : directoryFile.listFiles()) {
-	        if (!fileEntry.isDirectory()) {
-	            files.add(fileEntry.getName());
-	        }
-	    }
-	    
-	    return files;
-	}
+
 }
